b59535d562e739e5a170459cf4b9f5f3fbeed5a0
juntaow0 <wang_j2@denison.edu>
2021-05-30 00:32:42 -0700

    added ship but no control

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -11,6 +12,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -22,6 +24,7 @@ class AlienInvasion:
                 
             # Redraw screen
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # make the most recently drawn screen visible
             pygame.display.flip()
