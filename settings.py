2bbc342c9cbddf605abfac821fa80ef3c4ceb794
juntaow0 <wang_j2@denison.edu>
2021-06-03 18:16:36 -0700

    added bullet class and related settings

--- settings.py
+++ settings.py
@@ -11,3 +11,10 @@ class Settings:
 
         #ship settings
         self.ship_speed = 1.5
+
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 1
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        
